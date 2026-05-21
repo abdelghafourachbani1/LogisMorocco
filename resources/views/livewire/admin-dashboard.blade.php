2ed09d9dcb3a09a45d8a9f160e4e04e98012d8d7
Abdelghafour <achbaniabdelghafour8@gmail.com>
2026-05-21 10:26:19 +0100

    cleanup: remove all remaining Blade components, layouts, profile and Livewire views

--- resources/views/livewire/admin-dashboard.blade.php
+++
@@ -1,3 +0,0 @@
-<div style="width: 100vw; height: 100vh; overflow: hidden;">
-    <iframe src="http://localhost:3001" style="width: 100%; height: 100%; border: 0; display: block; margin: 0; padding: 0;"></iframe>
-</div>
