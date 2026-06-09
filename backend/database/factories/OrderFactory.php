6cb8f4204e30bc112b3ee8d0b5c0756c3221c6a7
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2026-06-09 14:45:24 +0100

    fixing bugs

--- backend/database/factories/OrderFactory.php
+++ backend/database/factories/OrderFactory.php
@@ -13,25 +13,25 @@ class OrderFactory extends Factory
 {
     public function definition(): array
     {
-        // $cities = [
-        //     'Casablanca' => ['Boulevard Anfa, Casablanca', 'Maarif, Casablanca', 'Sidi Maarouf, Casablanca', 'Oasis, Casablanca'],
-        //     'Rabat' => ['Agdal, Rabat', 'Hay Riad, Rabat', 'Avenue Mohammed V, Rabat'],
-        //     'Marrakech' => ['Gueliz, Marrakech', 'Medina, Marrakech', 'Hivernage, Marrakech'],
-        //     'Fez' => ['Ville Nouvelle, Fez', 'Route d\'Imouzzer, Fez'],
-        //     'Tangier' => ['Malabata, Tangier', 'Boulevard Mohamed V, Tangier']
-        // ];
+        $cities = [
+            'Casablanca' => ['Boulevard Anfa, Casablanca', 'Maarif, Casablanca', 'Sidi Maarouf, Casablanca', 'Oasis, Casablanca'],
+            'Rabat' => ['Agdal, Rabat', 'Hay Riad, Rabat', 'Avenue Mohammed V, Rabat'],
+            'Marrakech' => ['Gueliz, Marrakech', 'Medina, Marrakech', 'Hivernage, Marrakech'],
+            'Fez' => ['Ville Nouvelle, Fez', 'Route d\'Imouzzer, Fez'],
+            'Tangier' => ['Malabata, Tangier', 'Boulevard Mohamed V, Tangier']
+        ];
         
         $city = fake()->randomElement(array_keys($cities));
         $address = fake()->randomElement($cities[$city]);
 
-        // return [
-        //     'merchant_id' => User::factory()->state(['role' => 'merchant']),
-        //     'tracking_number' => 'LOG-' . strtoupper(Str::random(8)),
-        //     'customer_name' => fake()->name(),
-        //     'customer_phone' => '06' . fake()->numerify('########'),
-        //     'customer_address' => $address,
-        //     'amount_cod' => fake()->randomFloat(2, 100, 1500),
-        //     'status' => fake()->randomElement(['pending', 'in_transit', 'delivered', 'canceled', 'refused']),
-        // ];
+        return [
+            'merchant_id' => User::factory()->state(['role' => 'merchant']),
+            'tracking_number' => 'LOG-' . strtoupper(Str::random(8)),
+            'customer_name' => fake()->name(),
+            'customer_phone' => '06' . fake()->numerify('########'),
+            'customer_address' => $address,
+            'amount_cod' => fake()->randomFloat(2, 100, 1500),
+            'status' => fake()->randomElement(['pending', 'in_transit', 'delivered', 'canceled', 'refused']),
+        ];
     }
 }
